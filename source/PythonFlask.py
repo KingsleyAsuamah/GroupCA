61b5be120a6e81c15929a504bf418fcdb446a879
Jayabalaji Rajendran <jb20594@gmail.com>
2020-03-21 15:02:42 +0000

    Added Python Flask API to add data to MySQL Database

--- source/PythonFlask.py
+++ source/PythonFlask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -22,5 +22,21 @@ def home():
     return render_template('Home.html', data=data)
 
 
+# Python Flask API to add the data to MYSQL
+@app.route('/add', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        firstName = details['fname']
+        lastName = details['lname']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('Add.html')
+
+
+# Python Flask Main Function
 if __name__ == '__main__':
     app.run()
